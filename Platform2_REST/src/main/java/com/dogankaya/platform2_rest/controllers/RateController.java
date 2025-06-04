676c31bfb13a9b17a5db49f2b54381c3a763c87c
dogankaya <dogankaya2555@gmail.com>
2025-06-04 18:23:58 +0300

    Added an interface into Coordinator class and added coordinator controls via CoordinatorController.java and Telnet exceptions managed

--- Platform2_REST/src/main/java/com/dogankaya/platform2_rest/controllers/RateController.java
+++ Platform2_REST/src/main/java/com/dogankaya/platform2_rest/controllers/RateController.java
@@ -1,11 +1,14 @@
 package com.dogankaya.platform2_rest.controllers;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import rate.GetRateByTickerTypeRequest;
 import rate.RateDto;
 import com.dogankaya.platform2_rest.services.RateService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,7 +21,7 @@ public class RateController {
     }
 
     @GetMapping("/rates/{tickerType}")
-    public RateDto getRateByTickerType(GetRateByTickerTypeRequest request) {
-        return rateService.getRateByTickerType(request);
+    public List<RateDto> getRatesByTickerTypeList(@RequestParam List<GetRateByTickerTypeRequest> request) {
+        return rateService.getRatesByTickerTypeList(request);
     }
 }
